7a13f5e621246ee5639292c1e8593b82a422de72
sksdlin <sksdlin@gmail.com>
2018-07-22 16:03:25 +0530

    all dead functions implemented

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-var GAMESIZE_X = window.innerWidth;
-var GAMESIZE_Y = window.innerHeight;
+var GAMESIZE_X = 700;//window.innerWidth;
+var GAMESIZE_Y = 700;//window.innerHeight;
 var BLOCKSIZE = Math.min(GAMESIZE_X, GAMESIZE_Y) / 80;
 
 var Snake = function () {
@@ -20,14 +20,13 @@ var Snake = function () {
 
     }
 
-
     this.generateFood = function() {
         this.foodx = this.generateRandomNumber(GAMESIZE_X);
         this.foody = this.generateRandomNumber(GAMESIZE_Y);
 
     }
 
-    this.didntHitTheBody = function() {
+    this.notHitBody = function() {
         //for each is not working with head object array.
 
         for (let i = 0; i < this.body.length-1; i++){
@@ -44,14 +43,12 @@ var Snake = function () {
 
 
 
-    this.didnotHitTheBoundry = function () {
+    this.notHitBoundry = function () {
         //check if it hit the boundry
         // < 0 because snake shouldnot die on the fist row or first column of the game.
         if (this.head.x < 0 || this.head.y < 0 ||
             this.head.x >= GAMESIZE_X || this.head.y >= GAMESIZE_Y){
             console.log("sname hit boundry!");
-
-
             return false;
         }else {
             return true;
@@ -79,11 +76,30 @@ var Snake = function () {
         this.body.shift();
 
         //not hit the boundry
-        this.didntHitTheBody();
+        //this.didntHitTheBody();
 
         //not hit the body
         //this.didntHitTheBoundry();
 
+    }
+    this.generateFood();
+
+    this.restartGame = function () {
+        //console.log("restartGame called.");
+        //
+
+        while (this.body.length > 1){
+            this.body.pop();
+            console.log("poping");
+        }
+        this.head.x = 0;
+        this.head.y = 0;
+        this.moveToDir = "RIGHT";
+
+
+
+
+        //this.keepMoving();
     }
 }
 
@@ -118,24 +134,39 @@ function draw(){
     background(0);
 
     // draw food
-    fill(255);
-    stroke(255);
+    fill(0, 255, 0);
+    stroke(0, 255, 0);
 
     rect(snake.foodx ,snake.foody , BLOCKSIZE, BLOCKSIZE);
 
 
 
-    fill(0,255,29);
-    stroke(0);
+    fill(0, 255, 0);
+    stroke(0, 255, 0);
     for (var i = 0 ; i < snake.body.length; i++){
         rect(snake.body[i].x ,snake.body[i].y , BLOCKSIZE, BLOCKSIZE);
     }
 
     if (snake.head.x === snake.foodx && snake.head.y === snake.foody){
-        snake.body.push({x:snake.foodx, y:snake.foody});
+        snake.body.push({x:snake.foodx, y:snake.foody});    //food becomes part of body.
+        snake.keepMoving();
+        /*
+        head needs to move before checking the body.
+        because food was just added here and it becomes part of body.
+        */
+
         snake.generateFood();
+
+
     }
 
-    snake.keepMoving();
+
+    if (snake.notHitBody() && snake.notHitBoundry()){
+        snake.keepMoving();
+    }else {
+        snake.restartGame();
+
+    }
+
 
 }
